93a3a0ee78bf10cf07fea3c463ba8fd158d4c39d
oieioi <atsuinatsu.samuifuyu@gmail.com>
2019-05-07 14:45:49 +0900

    crawl specified file from CircleCI

--- lib/circleci_crawler.rb
+++ lib/circleci_crawler.rb
@@ -1,6 +1,39 @@
 require "circleci_crawler/version"
+require 'net/https'
+require 'json'
 
 module CircleciCrawler
-  class Error < StandardError; end
-  # Your code goes here...
+  class Crawler
+
+    def initialize(api_token:, project_path:, path:)
+      @api_token = api_token
+      @project_path = project_path
+      @path = path
+    end
+
+    def crawl
+      results = request("https://circleci.com/api/v1.1/project/#{@project_path}/latest/artifacts?circle-token=#{@api_token}")
+      target = results.find { |result| result[:path] == @path }
+
+      request("#{target[:url]}?circle-token=#{@api_token}", should_parse_to_json: false)
+    end
+
+    private
+
+    def request(url, should_parse_to_json: true)
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = uri.scheme == 'https'
+      req = Net::HTTP::Get.new(uri.to_s)
+      res = http.request(req)
+
+      raise "Failed to fetch #{uri}" unless res.is_a?(Net::HTTPSuccess)
+
+      if should_parse_to_json
+        JSON.parse(res.body, symbolize_names: true)
+      else
+        res.body
+      end
+    end
+  end
 end
